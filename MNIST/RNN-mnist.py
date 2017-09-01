58be8816e609c97818ddb3ec6e0e5460c6e01e57
Jaivarsan <jaiimmortal@gmail.com>
2017-09-01 20:37:18 +0530

    recurrent neural network

---
+++ MNIST/RNN-mnist.py
@@ -0,0 +1,79 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+# check https://stackoverflow.com/a/43555840
+# temporary solution https://github.com/tensorflow/tensorflow/issues/7778
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib import rnn
+#### Computation Graph ####
+
+mnist = input_data.read_data_sets('/tmp/data', one_hot = True)
+# one_hot returns the output as [1,0,0,0,0,0,0,0,0,0] if the image was '0'
+
+
+# output classes 0-9
+n_classes = 10
+
+# batch size for loading '128' features (here images) to the network at a time.
+batch_size = 128
+
+# how many epochs
+hm_epochs = 10
+
+chunk_size = 28
+n_chunks = 28
+rnn_size = 128
+
+# place holding variables
+# x is a matrix = height x width
+# but we flatten 28 x 28 into 784 string kind of thing.
+x = tf.placeholder('float', [None, n_chunks, chunk_size]) # input data
+y = tf.placeholder('float')
+
+def recurrent_neural_network(x):
+
+    # (input data * weights) + biases, used when input data = 0, mostly for making it dynamic
+    layer = {'weights':tf.Variable(tf.random_normal([rnn_size, n_classes])),
+             'biases':tf.Variable(tf.random_normal([n_classes]))}
+
+    x = tf.transpose(x, [1,0,2])
+    x = tf.reshape(x, [-1, chunk_size])
+    x = tf.split(x, n_chunks, 0)
+
+    lstm_cell = rnn.BasicLSTMCell(rnn_size)
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+
+    output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']
+
+    return output
+    #output is going to be the one_hot thing. Remember.
+
+#### Session ####
+
+def train_neural_network(x):
+    prediction = recurrent_neural_network(x)
+    # the cost function which is going to calculate how wrong we are with the prediction and actual value
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits = prediction,labels = y) )
+    # to minimize the difference in 'y' and 'prediction'
+    optimizer =  tf.train.AdamOptimizer().minimize(cost)
+
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+
+                _, c = sess.run([optimizer, cost], feed_dict= {x: epoch_x,y: epoch_y} )
+                epoch_loss += c
+            print('Epoch', epoch+1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy: ', accuracy.eval({x:mnist.test.images.reshape((-1, n_chunks, chunk_size)), y:mnist.test.labels}))
+
+train_neural_network(x)
